1c6861a843d290a0357b3dba802daa83a12df04f
cloudsauce <zaid_nz@yahoo.com>
2016-11-09 22:34:23 +1000

    Create secureboot.ps1

---
+++ secureboot.ps1
@@ -0,0 +1,14 @@
+#Uses Write-Menu from http://www.ps1code.com/single-post/2016/04/21/How-to-create-interactive-dynamic-Menu-in-PowerShell
+#
+$user = "administrator@vsphere.local"
+$password = "VMware1!"
+Connect-VIServer $viserver -user $user -Password $password
+$vmname = get-vm -name "ESXi 6.5"
+$SBchoices = "$true","$false"
+$SBtype = write-menu -menu $SBchoices -AddExit -HeaderColor Green -TextColor Yellow 
+
+
+$SecureBootValue = (Get-AdvancedSetting -Entity $vmname -Name "uefi.secureBoot.Enabled")
+Write-host "Secure Boot is: " $SecureBootValue
+Write-Host "Setting SecureBoot to True"
+$SecureBootValue = (Get-AdvancedSetting -Entity $vmname -Name "uefi.secureBoot.Enabled" | Set-AdvancedSetting -Value:$SBtype)
